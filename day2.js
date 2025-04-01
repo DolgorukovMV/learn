c0e10b80931b3fc57cecf725b733e9204944e2e8
Dolgorukov MV <iriy999@gmail.com>
2025-04-01 21:10:12 +0300

    День 2: Основы синтаксиса - переменные, операторы, арифметические операции

---
+++ day2.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 10;
+let greeting = "Привет, мир!";
+
+console.log("Значение а=", a);
+console.log("Значение b=", b);
+console.log("Значение greeting=", greeting);
+
+let sum = a +b; // сложение
+let difference = b - a; // вычитание
+let product = a * b; // умножение
+let quotient = b/a; // деление
+
+console.log("Сумма a и b", sum);
+console.log("вычитание a и b", difference);
+console.log("умножение a и b", product);
+console.log("деление a и b", quotient);
+
+let remainder = b % a;
+console.log("остаток от деления b на a =", remainder);
